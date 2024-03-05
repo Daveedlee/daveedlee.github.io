f5f9af8e76d8cd3ce52834f3f032da1060d1558a
jlee755 <jlee755@jh.edu>
2024-03-05 12:12:43 -0500

    folder reaarangement

---
+++ htmlcssjs/mod4_solution/SpeakHello.js
@@ -0,0 +1,17 @@
+(function (window) {
+
+let helloSpeaker = new Object();
+
+let speakWord = "Hello";
+
+helloSpeaker.speak = function(name){
+  console.log(speakWord + " " + name);
+};
+
+helloSpeaker.speakSimple = function(name){
+  return "Hello " + name;
+};
+
+window.helloSpeaker = helloSpeaker;
+
+})(window);
