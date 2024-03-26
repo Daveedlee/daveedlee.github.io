cb2f8f4c0f9695ab25c2c2b836972ae272d65963
David_Lee <jlee755@jh.edu>
2024-03-26 12:04:58 -0400

    Finishing up

--- angular/mod2_solution/app.js
+++ angular/mod2_solution/app.js
@@ -1,51 +1,57 @@
-angular.module('ShoppingListCheckOff', []);
+(function() {
+  'use strict';
 
-angular.module('ShoppingListCheckOff')
-.controller('ToBuyController', ['ShoppingListCheckOffService', function(ShoppingListCheckOffService) {
-  var toBuy = this;
-  
-  toBuy.items = ShoppingListCheckOffService.getToBuyItems();
-  
-  toBuy.buyItem = function(itemIndex) {
-    ShoppingListCheckOffService.buyItem(itemIndex);
-  };
-}])
-.controller('AlreadyBoughtController', ['ShoppingListCheckOffService', function(ShoppingListCheckOffService) {
-  var alreadyBought = this;
-  
-  alreadyBought.items = ShoppingListCheckOffService.getBoughtItems();
-}]);
+  angular.module('ShoppingListCheckOff', []);
 
+  angular.module('ShoppingListCheckOff')
+  .controller('ToBuyController', ['ShoppingListCheckOffService', function(ShoppingListCheckOffService) {
+      let toBuy = this;
 
-angular.module('ShoppingListCheckOff')
-.service('ShoppingListCheckOffService', function() {
-  var service = this;
-  
-  
-  var toBuyItems = [
-    { name: "cookies", quantity: 10, pricePerItem: 2 }
-    
-  ];
-  
-  var boughtItems = [];
-  
-  service.buyItem = function(itemIndex) {
-    var item = toBuyItems.splice(itemIndex, 1)[0];
-    boughtItems.push(item);
-  };
-  
-  service.getToBuyItems = function() {
-    return toBuyItems;
-  };
-  
-  service.getBoughtItems = function() {
-    return boughtItems;
-  };
-});
+      toBuy.items = ShoppingListCheckOffService.getToBuyItems();
 
-angular.module('ShoppingListCheckOff')
-.filter('angularDollars', function() {
-  return function(input) {
-    return '$$$' + input.toFixed(2);
-  };
-});
+      toBuy.buyItem = function(itemIndex) {
+          ShoppingListCheckOffService.buyItem(itemIndex);
+      };
+  }])
+
+  .controller('AlreadyBoughtController', ['ShoppingListCheckOffService', function(ShoppingListCheckOffService) {
+      let alreadyBought = this;
+
+      alreadyBought.items = ShoppingListCheckOffService.getBoughtItems();
+  }]);
+
+  angular.module('ShoppingListCheckOff')
+  .service('ShoppingListCheckOffService', function() {
+      let service = this;
+
+      let toBuyItems = [
+          { name: "cookies", quantity: 10, pricePerItem: 2 },
+          { name: "croissants", quantity: 20, pricePerItem: 5 },
+          { name: "milk", quantity: 10, pricePerItem: 1 },
+          { name: "coffee", quantity: 10, pricePerItem: 3 },
+          
+      ];
+
+      let boughtItems = [];
+
+      service.buyItem = function(itemIndex) {
+          let item = toBuyItems.splice(itemIndex, 1)[0];
+          boughtItems.push(item);
+      };
+
+      service.getToBuyItems = function() {
+          return toBuyItems;
+      };
+
+      service.getBoughtItems = function() {
+          return boughtItems;
+      };
+  });
+
+  angular.module('ShoppingListCheckOff')
+  .filter('angularDollars', function() {
+      return function(input) {
+          return '$$$' + input.toFixed(2);
+      };
+  });
+})();
